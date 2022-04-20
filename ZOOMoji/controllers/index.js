c7a9437f2268af4de39f55562cd1a120a752f526
Rebecca Hunt <rhunt3@nd.edu>
2022-04-20 18:38:57 -0400

    updated index.js with name

--- ZOOMoji/controllers/index.js
+++ ZOOMoji/controllers/index.js
@@ -1,5 +1,5 @@
 exports.index = function(req, res, next) {
-  res.render('index', { about: 'Welcome to our ZOOMoji!' , animal_name:
+  res.render('index', { about: 'Welcome to our ZOOMoji!' , name:
  'all memojis'});
 }
 
@@ -8,27 +8,27 @@ exports.zoom = function(req, res, next) {
 }
 
 exports.memojis = function(req, res, next) {
-   res.render('index', { about: 'See our memojis.', animal_name:'memojis'}); 
+   res.render('index', { about: 'See our memojis.', name:'memojis'}); 
 }
 
 exports.becca = function(req, res, next) {
    console.log("Clicked on becca button")
-   res.render('becca', { about: 'Lover of ducks, root beer, sweatshirts and long walks. In no particular order.', animal_name:'becca'});
+   res.render('becca', { about: 'Lover of ducks, root beer, sweatshirts and long walks. In no particular order.', name:'becca'});
 }
 
 exports.lily = function(req, res, next) {
    console.log("Clicked on lily button")
-   res.render('lily', { about: 'Lover of theater, all of the grandma things (knitting, sewing, reading), and coffee with lots of sugar. All number one.' , animal_name:'lily'}); 
+   res.render('lily', { about: 'Lover of theater, all of the grandma things (knitting, sewing, reading), and coffee with lots of sugar. All number one.' , name:'lily'}); 
 }
 
 exports.bridget = function(req, res, next) {
    console.log("Clicked on bridget button")
-   res.render('bridget', { about: 'Lover of Jesus, Sam\'s Club pizza, a good tan and the color green. In that order.' , animal_name:'bridget'}); 
+   res.render('bridget', { about: 'Lover of Jesus, Sam\'s Club pizza, a good tan and the color green. In that order.' , name:'bridget'}); 
 }
 
 exports.dom = function(req, res, next) {
    console.log("Clicked on dom button")
-   res.render('dom', { about: 'Monkeys live in trees, grasslands, mountains, forests, and on high plains. Each monkey has its own unique fingerprints.' , animal_name:'dom'}); 
+   res.render('dom', { about: 'Monkeys live in trees, grasslands, mountains, forests, and on high plains. Each monkey has its own unique fingerprints.' , name:'dom'}); 
 }
 
 //exports.map = function(req, res, next) {
