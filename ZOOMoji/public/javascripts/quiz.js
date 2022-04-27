60f204f39d5078021afa63863bf1ce5f27f60086
Lillian Pierson <lpierson@nd.edu>
2022-04-27 12:13:17 -0400

    Updates to quiz

--- ZOOMoji/public/javascripts/quiz.js
+++ ZOOMoji/public/javascripts/quiz.js
@@ -1,85 +1,114 @@
-/* script for quiz functionality */
+(function(){
+    function buildQuiz(){
+        const output = [];
 
-// select and ask questions from pool
+        questions.forEach((currentQuestion, questionNumber) => {
 
-function getQuestions(){
-    const questionCombos = ["012", "013", "014", "123", "124", "134", "023", "024", "034", "234"];
-
-    let questions = questionCombos[Math.floor(Math.random() * 9)];
-
-    for (let i = 0; i < 3; i++) {
-        q = questions.charAt(i);
-        switch(q) {
-            case '0':
-                console.log("dog ?");
-                break;
-            case '1':
-                console.log("dorm ?");
-                break;
-            case '2':
-                console.log("food ?");
-                break;
-            case '3':
-                console.log("ND trad ?");
-                break;
-            case '4':
-                console.log("swift ?");
-                break;
-        }
-    }
-
-    return questions;
-};
-
-function getResults(answers){
-// get answer
-//let answers = "abc";
-
-    // compute score
-    let aCount = 0;
-    let bCount = 0;
-    let cCount = 0;
-    let dCount = 0;
-
-    for (let j = 0; j < 3; j++) {
-        if (answers.charAt(j) == 'a') {
-            aCount++;
-        }
-        else if (answers.charAt(j) == 'b') {
-            bCount++;
-        }
-        else if (answers.charAt(j) == 'c') {
-            cCount++;
-        }
-        else if (answers.charAt(j) == 'd') {
-            dCount++;
-        }
-        else {
-            console.log("invalid input");
-        }
-    }
-
-    // display results
-    let winner = Math.max(aCount, bCount, cCount, dCount);
-    if (winner == aCount) {
-        console.log("you are most like becca");
-    }
-
-    else if (winner == bCount) {
-        console.log("you are most like bridget");
-    }
-
-    else if (winner == cCount) {
-        console.log("you are most like lily");
-    }
-
-    else if (winner == dCount) {
-        console.log("you are most like dom");
-    }
-
-    else {
-        console.log("we don't know who you are");
-    }
+            const answers = [];
     
-    return winner;
-}
+            for(letter in currentQuestion.answers){
+                answers.push('input.form-control(type=\'radio\', name=\'q'+ i + '\', value=\'' + letter + '\')\n label(for \'' + letter + '\')' + questions[questionNums[i]].answers[letter]);
+            }
+
+            
+            output.push('div (class="question")' + questions[questionNums[i].question + '\n div (class="answers")' + answers.join(''));
+        });
+
+        quizContainer.innerHTML = output.join('');
+    }
+
+    function showResults(){
+    
+        const answerContainers = quizContainer.querySelectorAll('
+
+        var userAnswer = '';
+        var aCount = 0;
+        var bCount = 0;
+        var cCount = 0;
+        var dCount = 0;
+
+        for(var i=0; i < questionNums.length; i++){
+            userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+
+            if(userAnswer == 'a'){
+                aCount++;
+            }else if (userAnswer == 'b'){
+                bCount++;
+            }else if (userAnswer == 'c'){
+                cCount++;
+            }else if (userAnswer == 'd'){
+
+            }
+
+        }
+
+        let winner = Math.max(aCount, bCount, cCount, dCount);
+
+        if(winner == aCount){
+            resultsContainer.innerHTML = 'You are most like Becca!';
+        }else if (winner == bCount){
+            resultsContainer.innerHTML = 'You are most like Bridget!';
+        }else if (winner == cCount){
+            resultsContainer.innerHTML = 'You are most like Lily!';
+        }else if (winner == dCount){
+            resultsContainer.innerHTML = 'You are most like Dom!';
+        }
+    }   
+            
+    const  quizContainer = document.getElementById('quiz');
+    const resultsContainer = document.getElementById('results');
+    const submitButton = document.getElementById('submit');
+                
+    const questions = [
+        {
+            question: "What is your favorite dog?",
+            answers: {
+                a: 'I\'m a cat person'
+                b: 'My dog'
+                c: 'Any dog that will let me pet it'
+                d: 'Beagles'
+            }
+        },
+        {
+            question: "What dorm do you live in",
+            answers: {
+                a: 'Off-campus'
+                b: 'Cav'
+                c: 'PE'
+                d: 'Siggy, baby'
+            }
+        },
+        {
+            question: "What is your favorite food to have on campus?",
+            answers: {
+                a: 'Smashburger'
+                b: 'Starbucks'
+                c: 'Modern Market'
+                d: 'Subway'
+            }
+        },
+        {
+            question: "What is your favorite ND tradition?",
+            answers: {
+                a: 'ND Day'
+                b: 'Keenan Revue'
+                c: 'The Meritocracy'
+                d: 'Football Games'
+            }
+        },
+        {
+            question: "What is your favorite Taylor Swift song?",
+            answers: {
+                a: 'Paper Rings'
+                b: 'Picture to Burn'
+                c: 'London Boy'
+                d: 'I Knew You Were Trouble'
+            }
+        }];
+
+
+    buildQuiz();
+
+    submitButton.addEventListener('click', showResults);
+
+})();
---
+++ ZOOMoji/public/javascripts/quiz1.js
@@ -0,0 +1,85 @@
+/* script for quiz functionality */
+
+// select and ask questions from pool
+
+function getQuestions(){
+    const questionCombos = ["012", "013", "014", "123", "124", "134", "023", "024", "034", "234"];
+
+    let questions = questionCombos[Math.floor(Math.random() * 9)];
+
+    for (let i = 0; i < 3; i++) {
+        q = questions.charAt(i);
+        switch(q) {
+            case '0':
+                console.log("dog ?");
+                break;
+            case '1':
+                console.log("dorm ?");
+                break;
+            case '2':
+                console.log("food ?");
+                break;
+            case '3':
+                console.log("ND trad ?");
+                break;
+            case '4':
+                console.log("swift ?");
+                break;
+        }
+    }
+
+    return questions;
+};
+
+function getResults(answers){
+// get answer
+//let answers = "abc";
+
+    // compute score
+    let aCount = 0;
+    let bCount = 0;
+    let cCount = 0;
+    let dCount = 0;
+
+    for (let j = 0; j < 3; j++) {
+        if (answers.charAt(j) == 'a') {
+            aCount++;
+        }
+        else if (answers.charAt(j) == 'b') {
+            bCount++;
+        }
+        else if (answers.charAt(j) == 'c') {
+            cCount++;
+        }
+        else if (answers.charAt(j) == 'd') {
+            dCount++;
+        }
+        else {
+            console.log("invalid input");
+        }
+    }
+
+    // display results
+    let winner = Math.max(aCount, bCount, cCount, dCount);
+    if (winner == aCount) {
+        console.log("you are most like becca");
+    }
+
+    else if (winner == bCount) {
+        console.log("you are most like bridget");
+    }
+
+    else if (winner == cCount) {
+        console.log("you are most like lily");
+    }
+
+    else if (winner == dCount) {
+        console.log("you are most like dom");
+    }
+
+    else {
+        console.log("we don't know who you are");
+    }
+    
+    return winner;
+}
